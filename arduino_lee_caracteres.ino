8bdad56fea12615f6c847747050d2372b3ba1d26
thomas484 <42107952+thomas484@users.noreply.github.com>
2022-10-15 06:11:23 -0300

    Update arduino_lee_caracteres.ino

--- arduino_lee_caracteres.ino
+++ arduino_lee_caracteres.ino
@@ -3,45 +3,45 @@
   until it gets some data in. Then it waits for a byte in the serial port, and
   sends three sensor values whenever it gets a byte in.
 */
-int firstSensor = 0;    // first analog sensor
-int secondSensor = 0;   // second analog sensor
-int thirdSensor = 0;    // digital sensor
-int inByte = 0;         // incoming serial byte
+String palabra = "";
+
 
 void setup() {
   // start serial port at 9600 bps:
+    pinMode(LED_BUILTIN, OUTPUT);  
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
-  }
-
-  pinMode(2, INPUT);   // digital sensor is on digital pin 2
-  establishContact();  // send a byte to establish contact until receiver responds
+  } 
+  establishContact();  // send a byte to establish contact until receiver responds 
 }
 
 void loop() {
-  // if we get a valid byte, read analog ins:
-  if (Serial.available() > 0) {
-    // get incoming byte:
-    inByte = Serial.read();
-    // read first analog input, divide by 4 to make the range 0-255:
-    firstSensor = analogRead(A0) / 4;
-    // delay 10ms to let the ADC recover:
-    delay(10);
-    // read second analog input, divide by 4 to make the range 0-255:
-    secondSensor = analogRead(1) / 4;
-    // read switch, map it to 0 or 255L
-    thirdSensor = map(digitalRead(2), 0, 1, 0, 255);
-    // send sensor values:
-    Serial.write(firstSensor);
-    Serial.write(secondSensor);
-    Serial.write(thirdSensor);
+ 
+  if (Serial.available()){
+palabra = leer_puerto();
+    if(palabra.toInt()==1) //convierte la cadena a el numero entero correspondiente
+  digitalWrite(LED_BUILTIN, HIGH);
+  else if(palabra.toInt()==2)
+   digitalWrite(LED_BUILTIN, LOW);
+   Serial.print(palabra);
   }
 }
 
 void establishContact() {
   while (Serial.available() <= 0) {
-    Serial.print('A');   // send a capital A
-    delay(300);
+    Serial.print('A');   // send a capital A MANDA CARACTERES A HASTA QUE RECIBA ALGUN DATO DE INICIO DEL OTRO PROGRAMA EN C PARA LA INTERFAZ 
+    delay(100);
   }
 }
+String leer_puerto(void){ //lee_el_puerto();
+    String suma_de_char = "";
+   char char_individual = Serial.read(); //lee el el valor desde el puerto
+    if (int(char_individual)!=-1){ //pregunta si el char esta sin información nueva haciendo un cast a int 
+      suma_de_char = suma_de_char + char_individual;
+    }
+    if (char_individual=='\n') //cuando desde la pc le damos enter detecta el fin de linea
+     return suma_de_char;
+  }
+  
+
