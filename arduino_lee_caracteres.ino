5bfc188e1bf690e2d082d830d23ef6ba2def1fb7
thomas484 <42107952+thomas484@users.noreply.github.com>
2022-10-15 03:56:01 -0300

    Update arduino_lee_caracteres.ino

--- arduino_lee_caracteres.ino
+++ arduino_lee_caracteres.ino
@@ -1,33 +1,47 @@
-String vectorCaracteres;
-boolean TransmisionCompleta = false;
+/*
+ This program sends an ASCII A (byte of value 65) on startup and repeats that
+  until it gets some data in. Then it waits for a byte in the serial port, and
+  sends three sensor values whenever it gets a byte in.
+*/
+int firstSensor = 0;    // first analog sensor
+int secondSensor = 0;   // second analog sensor
+int thirdSensor = 0;    // digital sensor
+int inByte = 0;         // incoming serial byte
 
 void setup() {
-
-
+  // start serial port at 9600 bps:
   Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
 
-
+  pinMode(2, INPUT);   // digital sensor is on digital pin 2
+  establishContact();  // send a byte to establish contact until receiver responds
 }
 
 void loop() {
-
-  //Ciclo infinito
-  if (TransmisionCompleta) {
-    //vectorCaracteres //Los datos ingresados se encuentran en esta variable
-    vectorCaracteres = "";  //Limpiar el String
-    TransmisionCompleta = false;  //Limpiar la bandera
+  // if we get a valid byte, read analog ins:
+  if (Serial.available() > 0) {
+    // get incoming byte:
+    inByte = Serial.read();
+    // read first analog input, divide by 4 to make the range 0-255:
+    firstSensor = analogRead(A0) / 4;
+    // delay 10ms to let the ADC recover:
+    delay(10);
+    // read second analog input, divide by 4 to make the range 0-255:
+    secondSensor = analogRead(1) / 4;
+    // read switch, map it to 0 or 255L
+    thirdSensor = map(digitalRead(2), 0, 1, 0, 255);
+    // send sensor values:
+    Serial.write(firstSensor);
+    Serial.write(secondSensor);
+    Serial.write(thirdSensor);
   }
 }
 
-void serialEvent() {
-  while (Serial.available()) {
-
-    char CharEntrada = Serial.read(); //Leer un byte del puerto serial
-
-    vectorCaracteres += CharEntrada;  //Agregar el char anterior al string
-
-    if (CharEntrada == '\n') {  //Si se detecta un fin de linea
-      TransmisionCompleta = true;  //Se indica al programa que el usuario termino de ingresar la informacion
-    }
+void establishContact() {
+  while (Serial.available() <= 0) {
+    Serial.print('A');   // send a capital A
+    delay(300);
   }
 }
